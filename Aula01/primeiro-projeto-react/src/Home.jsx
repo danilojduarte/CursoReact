406602a5cfd80349ebc031b57ae027ee15c95728
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2023-10-14 03:23:05 -0300

    Add useState e itens a lista

--- Aula01/primeiro-projeto-react/src/Home.jsx
+++ Aula01/primeiro-projeto-react/src/Home.jsx
@@ -1,16 +1,24 @@
 import { useRef } from 'react'
+import { useState } from 'react'
+
+
 
 function Home() {
   const inputRef = useRef()
 
   function cliqueiNoBotao() {
-    console.log(  inputRef.current.value )
+    produtos.push(inputRef.current.value)
   }
   return (
     <div>
       <h1>Lista de Compras</h1>
       <input placeholder="produto" ref={inputRef}/>
       <button onClick={cliqueiNoBotao}>Adicionar</button>
+
+      {
+        produtos.map((produto) => (
+          <div>{produto}</div>
+        ))}
     </div>
   )
 }
