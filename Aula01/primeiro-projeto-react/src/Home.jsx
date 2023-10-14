57f193266e98fce72b1d8f777ac0cc9ea375474d
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2023-10-14 02:11:12 -0300

    Function JavaScript add ao projeto

--- Aula01/primeiro-projeto-react/src/Home.jsx
+++ Aula01/primeiro-projeto-react/src/Home.jsx
@@ -1,10 +1,14 @@
 
 
 function Home() {
+  function cliqueiNoBotao() {
+    console.log("botao foi clicado")
+  }
   return (
     <div>
       <h1>Lista de Compras</h1>
       <input placeholder="produto" />
+      <button onClick={cliqueiNoBotao}>Adicionar</button>
     </div>
   )
 }
